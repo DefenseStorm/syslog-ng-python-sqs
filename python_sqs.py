fb9908ea24f096f7b4079369effe52de50f16ef6
Brandon Smith <brandon@praesid.io>
2014-11-03 13:14:29 -0800

    Add GPL

--- python_sqs.py
+++ python_sqs.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+# Copyright 2014 Praesidio Inc.
+# Written by: Brandon Smith <brandon@praesid.io>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License version 2 for
+# more details.
+#
+# You should have received a copy of the GNU General Public License version 2
+# along with this program; see the file COPYING.  If not, see
+# <https://gnu.org/licenses/old-licenses/gpl-2.0.html>.
+#
+
 from __future__ import with_statement
 from threading import Timer, Lock
 from collections import deque
