78c487abd66538a1c2d71bb3e8e9751f514dde77
Brandon Smith <brandon@praesid.io>
2014-11-26 12:20:43 -0800

    Try a few encodings instead of assuming ascii

--- python_sqs_stdin.py
+++ python_sqs_stdin.py
@@ -4,12 +4,23 @@ import python_sqs
 
 log = None
 line = None
+encodings = ('UTF-8', 'WINDOWS-1252', 'ISO-8859-1')
 try:
     python_sqs.init(json_input=True)
     log = logging.getLogger(__name__)
     line = sys.stdin.readline()
     while line:
-        python_sqs.queue(line.strip())
+        line = line.strip()
+        for encoding in encodings:
+            try:
+                line = unicode(line, encoding)
+                break
+            except ValueError:
+                pass
+        else:
+            log.debug("Falling back to ascii w/replace")
+            line = unicode(line, errors='replace')
+        python_sqs.queue(line)
         line = sys.stdin.readline()
 except e:
     if log:
