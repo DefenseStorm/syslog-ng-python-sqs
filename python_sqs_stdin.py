59bb7bd57ccdd843802335f6dd20c5a0b26ac072
Brandon Smith <brandon@praesid.io>
2014-11-25 19:08:28 -0800

    Revert "Turn on json-python-json until I fix it"
    This reverts commit cd7aee8852676dc481003fc845aba26759a7e848.

--- python_sqs_stdin.py
+++ python_sqs_stdin.py
@@ -5,11 +5,11 @@ import python_sqs
 log = None
 line = None
 try:
-    python_sqs.init(json_input=False)
+    python_sqs.init(json_input=True)
     log = logging.getLogger(__name__)
     line = sys.stdin.readline()
     while line:
-        python_sqs.queue(json.loads(line.strip()))
+        python_sqs.queue(line.strip())
         line = sys.stdin.readline()
 except e:
     if log:
